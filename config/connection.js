5706124a1694e072bd935dd94f28af5fa4db11ad
Beefs4000 <103645633+Beefs4000@users.noreply.github.com>
2022-08-25 12:38:57 +0800

    heroku test v2

--- config/connection.js
+++ config/connection.js
@@ -4,12 +4,12 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize('heroku_f13cfa179ca80c3', 'b264676882cda6', '2c697f2d', {
-      host: 'us-cdbr-east-06.cleardb.net',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql',
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  });
 
 module.exports = sequelize;
